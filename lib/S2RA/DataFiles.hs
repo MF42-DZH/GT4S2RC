2f61adc40164b4e9dff5cf0b60a4a931d7070fac
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2025-03-25 13:24:01 +0000

    Remove a few more bits of track randomizer stuff.

--- lib/S2RA/DataFiles.hs
+++ lib/S2RA/DataFiles.hs
@@ -29,12 +29,6 @@ data DataFile
   | RaceList
   | FuncList
   | RaceNames
-  | TrackIds
-  | TrackNames
-  | TrackClasses
-  | SingleRaceIDs
-  | SingleRaceNames
-  | SingleRaceClasses
   deriving (Show, Read, Eq, Ord, Enum, Bounded)
 
 dataFilePath :: DataFile -> String
@@ -46,12 +40,6 @@ dataFilePath NecessityValues   = "Data/NECESSITYVALUES.txt"
 dataFilePath RaceList          = "Data/RACELIST.txt"
 dataFilePath FuncList          = "Data/FUNCLIST.txt"
 dataFilePath RaceNames         = "Data/RACENAMES.txt"
-dataFilePath TrackIds          = "Data/TRACKIDS.txt"
-dataFilePath TrackNames        = "Data/TRACKNAMES.txt"
-dataFilePath TrackClasses      = "Data/TRACKCLASS.txt"
-dataFilePath SingleRaceIDs     = "Data/SINGLERACEIDS.txt"
-dataFilePath SingleRaceNames   = "Data/SINGLERACENAMES.txt"
-dataFilePath SingleRaceClasses = "Data/SINGLERACECLASS.txt"
 
 loadCarsAndEvents :: IO ([Car], [Event])
 loadCarsAndEvents = (,)
